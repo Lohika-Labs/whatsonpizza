57767e35bdd1ba6661bb334a11e5a61642edacaf
Bohdan Turkynewych <bturkynewych@lohika.com>
2018-03-09 16:01:38 +0200

    Readme + deps + strict python3/gunicorn check

---
+++ whatsonpizza_classifier/README.md
@@ -0,0 +1,16 @@
+# Intro
+
+Pizza images classifier
+
+## Installation
+
+`make deps`
+
+## Running
+
+`./run.sh`
+
+
+## Development
+
+`./site.py`
--- whatsonpizza_classifier/run.sh
+++ whatsonpizza_classifier/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-curdir=$(pwd)
-
-cd $(dirname $0)
-
-PYTHONPATH=../ gunicorn -c etc/gunicorn.conf.py whatsonpizza_classifier.site:app
-
-cd ${curdir}
+if [ $(python3 -c 'import gunicorn; print(gunicorn.SERVER_SOFTWARE.startswith("gunicorn"))') != "True" ]; then #'
+    echo "Has to run on Python3, sorry."
+    exit 127
+else
+    curdir=$(pwd)
+    cd $(dirname $0)
+    PYTHONPATH=../ gunicorn -c etc/gunicorn.conf.py whatsonpizza_classifier.site:app
+    cd ${curdir}
+fi
