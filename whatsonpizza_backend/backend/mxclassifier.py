b9434bc89a565d71fe7e6653efd0f0829ac4b5e6
Bohdan Turkynewych <bturkynewych@lohika.com>
2018-04-19 19:02:44 +0300

    updated tensorflow to 1.7.0

--- whatsonpizza_backend/backend/mxclassifier.py
+++ whatsonpizza_backend/backend/mxclassifier.py
@@ -1,62 +1,66 @@
 #-*- coding: utf-8 -*-
+""" MXNet recognition module """
 
 import os
-import mxnet as mx
-import cv2
-import numpy as np
 import json
-# define a simple data batch
+
 from collections import namedtuple, OrderedDict
-from PIL import Image
+
+import cv2
+import mxnet as mx
+import numpy as np
+
 
 from .common import PROJECT_BASE
+from .logger import logger
+
 
 MODEL_DIR = os.path.join(PROJECT_BASE, 'models', 'mxnet')
 MODEL = os.path.join(MODEL_DIR, 'resnet-50')
 MODEL_LABELS = os.path.join(MODEL_DIR, 'label_map.json')
 
 
-Batch = namedtuple('Batch', ['data'])
-Image.Image.tostring = Image.Image.tobytes
+class MXNetBackend(object):
+    """ MXNet classifier """
+    def __init__(self):
+        self.batch = namedtuple('Batch', ['data'])
+        sym, arg_params, aux_params = mx.model.load_checkpoint(MODEL, 50)
+        self.mod = mx.mod.Module(symbol=sym, context=mx.cpu(), label_names=None)
+        self.mod.bind(for_training=False, data_shapes=[('data', (1, 3, 224, 224))],
+                      label_shapes=self.mod._label_shapes)  # pylint:disable=protected-access
+        self.mod.set_params(arg_params, aux_params, allow_missing=True)
 
-ctx = mx.cpu()
+    @staticmethod
+    def get_cats():
+        """ Return list of categories """
+        categories = []
+        taxonomy = json.loads(open(MODEL_LABELS, 'r').read())
+        for _, category in OrderedDict(taxonomy).items():
+            categories.append(category)
+        return categories
 
-sym, arg_params, aux_params = mx.model.load_checkpoint(MODEL, 50)
-mod = mx.mod.Module(symbol=sym, context=mx.cpu(), label_names=None)
-mod.bind(for_training=False, data_shapes=[('data', (1, 3, 224, 224))],
-         label_shapes=mod._label_shapes)
-mod.set_params(arg_params, aux_params, allow_missing=True)
+    @staticmethod
+    def get_image(fname):
+        """ Create image object from file """
+        img = cv2.cvtColor(cv2.imread(fname), cv2.COLOR_BGR2RGB)  # pylint:disable=no-member
+        if img is None:
+            return None
+        img = cv2.resize(img, (224, 224))  # pylint:disable=no-member
+        img = np.swapaxes(img, 0, 2)
+        img = np.swapaxes(img, 1, 2)
+        img = img[np.newaxis, :]
+        return img
 
-
-def get_cats():
-    cats = []
-    taxonomy = json.loads(open(MODEL_LABELS, 'r').read())
-    for _, v in OrderedDict(taxonomy).items():
-        cats.append(v)
-    return cats
-
-
-def get_image(fname):
-    img = cv2.cvtColor(cv2.imread(fname), cv2.COLOR_BGR2RGB)
-    if img is None:
-        return None
-    img = cv2.resize(img, (224, 224))
-    img = np.swapaxes(img, 0, 2)
-    img = np.swapaxes(img, 1, 2)
-    img = img[np.newaxis, :]
-    return img
-
-
-def predict(fname):
-    img = get_image(fname)
-    mod.forward(Batch([mx.nd.array(img)]))
-    prob = mod.get_outputs()[0].asnumpy()
-    prob = np.squeeze(prob)
-    acc = np.argsort(prob)[::-1]
-    cat = get_cats()
-    print (cat)
-    ALL = []
-    for i in range(9):
-        tup = (cat[i], prob[i])
-        ALL.append(tup)
-    return ALL
+    def mxnet_predict_image(self, fname):
+        """ Predict category using MXNet """
+        img = self.get_image(fname)
+        self.mod.forward(self.batch([mx.nd.array(img)]))
+        prob = self.mod.get_outputs()[0].asnumpy()
+        prob = np.squeeze(prob)
+        cat = self.get_cats()
+        logger.warning('MXNet categories: %s', cat)
+        results = []
+        for idx in range(9):
+            tup = (cat[idx], prob[idx])
+            results.append(tup)
+        return results
