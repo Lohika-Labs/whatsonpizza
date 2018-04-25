70cb86ea3b732446d4d12f8e14bafbd666d1bb7e
David Mayboroda <david.ftzi@gmail.com>
2018-04-26 01:06:59 +0200

    finetune with inception

--- finetune.py
+++ finetune.py
@@ -48,7 +48,7 @@ def get_model(prefix, epoch):
     download(prefix + '-%04d.params' % (epoch,))
 
 
-def get_finetune(symbol, arg_params, num_classes, layer_name="flatten"):
+def get_finetune(symbol, arg_params, num_classes, layer_name="Flatten"):
     all_layers = symbol.get_internals()
     net = all_layers[layer_name]
     net = mx.symbol.FullyConnected(data=net, num_hidden=num_classes, name='fc1')
