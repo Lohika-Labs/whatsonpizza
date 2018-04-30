b36dd146f166d21bf82fbc62caad979ce781789d
David Mayboroda <david.ftzi@gmail.com>
2018-04-30 16:47:55 +0200

    changes in finetune

--- finetune.py
+++ finetune.py
@@ -1,14 +1,7 @@
 import mxnet as mx
-import os, sys
 import logging
-
 head = '%(asctime)-15s %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=head)
-
-if sys.version_info[0] >= 3:
-    from urllib.request import urlretrieve
-else:
-    from urllib import urlretrieve
 
 num_classes = 10
 batch_per_gpu = 64
@@ -16,13 +9,7 @@ num_gpus = 2
 batch_size = batch_per_gpu * num_gpus
 
 
-def download(url):
-    filename = url.split("/")[-1]
-    if not os.path.exists(filename):
-        urlretrieve(url, filename)
-
-
-def get_iterators(batch_size, data_shape=(3, 224, 224)):
+def get_iterators(batch_size, data_shape=(3, 256, 256)):
     train = mx.io.ImageRecordIter(
         path_imgrec='./data-train.rec',
         data_name='data',
@@ -43,14 +30,9 @@ def get_iterators(batch_size, data_shape=(3, 224, 224)):
     return train, val
 
 
-def get_model(prefix, epoch):
-    download(prefix + '-symbol.json')
-    download(prefix + '-%04d.params' % (epoch,))
-
-
-def get_finetune(symbol, arg_params, num_classes, layer_name="flatten_output"):
+def do_finetune(symbol, arg_params, num_classes):
     all_layers = symbol.get_internals()
-    net = all_layers[layer_name]
+    net = all_layers["flatten_output"]
     net = mx.symbol.FullyConnected(data=net, num_hidden=num_classes, name='fc1')
     net = mx.symbol.SoftmaxOutput(data=net, name='softmax')
     new_args = dict({k: arg_params[k] for k in arg_params if 'fc1' not in k})
@@ -66,7 +48,7 @@ def fit(symbol, arg_params, aux_params, train, val, batch_size, num_gpus):
             aux_params=aux_params,
             allow_missing=True,
             batch_end_callback=mx.callback.Speedometer(batch_size, 1),
-            epoch_end_callback=mx.callback.do_checkpoint("inception", 25),
+            epoch_end_callback=mx.callback.do_checkpoint("Inception", 25),
             kvstore='device',
             optimizer='sgd',
             optimizer_params={'learning_rate': 0.01},
@@ -76,9 +58,8 @@ def fit(symbol, arg_params, aux_params, train, val, batch_size, num_gpus):
     return mod.score(val, metric)
 
 
-#get_model('http://data.dmlc.ml/mxnet/models/imagenet/inception-bn/', 0)
 sym, arg_params, aux_params = mx.model.load_checkpoint('Inception-BN', 0)
 (train, val) = get_iterators(batch_size)
-(new_sym, new_args) = get_finetune(sym, arg_params, num_classes)
+(new_sym, new_args) = do_finetune(sym, arg_params, num_classes)
 mod_score = fit(new_sym, new_args, aux_params, train, val, batch_size, num_gpus)
 print (mod_score)
