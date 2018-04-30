e5cd5c5c240066027a460e904ec9b2479f04be3a
David Mayboroda <david.ftzi@gmail.com>
2018-04-30 12:04:48 +0200

    changed training set

--- imgtool.sh
+++ imgtool.sh
@@ -4,5 +4,5 @@ CMD="python3 -m mxnet.tools.im2rec"
 
 ${CMD} --list --recursive --num-thread 16 data-train data/train/
 ${CMD} --list --recursive --num-thread 16 data-val data/orig/
-${CMD} --resize 250 --quality 90 --num-thread 16 data-val data/orig/
-${CMD} --resize 250 --quality 90 --num-thread 16 data-train data/train/
+${CMD} --resize 256 --quality 90 --num-thread 16 data-val data/orig/
+${CMD} --resize 256 --quality 90 --num-thread 16 data-train data/train/
