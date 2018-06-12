9588929f126a2805d2211b36e36ff03ed857288c
David Mayboroda <david.ftzi@gmail.com>
2018-06-12 21:11:21 +0200

    change layers position

--- finetune.py
+++ finetune.py
@@ -33,9 +33,10 @@ def get_iterators(batch_size, data_shape=(3, 299, 299)):
 def do_finetune(symbol, arg_params):
     all_layers = symbol.get_internals()
     net = all_layers["flatten_output"]
+    net = mx.symbol.Pooling(data=net, pool_type='max', stride=(1,1))
+    net = mx.symbol.Dropout(data=net, p=0.7, name='dp', mode='always')
     net = mx.symbol.FullyConnected(data=net, num_hidden=num_classes, name='fc1')
     net = mx.symbol.SoftmaxOutput(data=net, name='softmax')
-    net = mx.symbol.Dropout(data=net, p=0.7, name='dp', mode='always')
     new_args = dict({k: arg_params[k] for k in arg_params if 'fc1' not in k})
     return net, new_args
 
