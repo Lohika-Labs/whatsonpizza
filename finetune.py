65b0a937550276e3c1433bdb0c9fcb9ff20290dd
David Mayboroda <david.ftzi@gmail.com>
2018-06-18 01:10:10 +0200

    added relu

--- finetune.py
+++ finetune.py
@@ -33,8 +33,8 @@ def get_iterators(batch_size, data_shape=(3, 299, 299)):
 def do_finetune(symbol, arg_params):
     all_layers = symbol.get_internals()
     net = all_layers["flatten_output"]
-    net = mx.symbol.Dropout(data=net, p=0.7, name='dp', mode='always')
     net = mx.sym.Activation(data=net, name='relu1', act_type="relu")
+    net = mx.symbol.Dropout(data=net, p=0.7, name='dp', mode='always')
     net = mx.symbol.FullyConnected(data=net, num_hidden=num_classes, name='fc1')
     net = mx.symbol.SoftmaxOutput(data=net, name='softmax')
     new_args = dict({k: arg_params[k] for k in arg_params if 'fc1' not in k})
