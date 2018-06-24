ae57fe2ca819d8929bc95b52b4b0f45bae814872
David Mayboroda <david.ftzi@gmail.com>
2018-06-24 16:25:45 +0200

    added pooling (average)

--- finetune.py
+++ finetune.py
@@ -36,6 +36,7 @@ def do_finetune(symbol, arg_params):
     net = all_layers["flatten_output"]
     net = mx.sym.Activation(data=net, name='relu1', act_type="relu")
     net = mx.symbol.Dropout(data=net, p=0.7, name='dp', mode='always')
+    net = mx.symbol.Pooling(data=net, pool_type='avg')
     net = mx.symbol.FullyConnected(data=net, num_hidden=num_classes, name='fc1')
     net = mx.symbol.SoftmaxOutput(data=net, name='softmax')
     new_args = dict({k: arg_params[k] for k in arg_params if 'fc1' not in k})
