6809607d081c41cf89a6730e95dc7bd6f1f8a6f7
Vadym Bartko <vbartko@lohika.com>
2018-07-23 13:08:25 +0300

    add some comments

--- tf_dev/inception_retrain.py
+++ tf_dev/inception_retrain.py
@@ -5,7 +5,7 @@ import os.path
 from PIL import ImageFile
 from keras import optimizers
 from keras.applications.inception_v3 import InceptionV3
-from keras.callbacks import ModelCheckpoint, EarlyStopping, LearningRateScheduler
+from keras.callbacks import ModelCheckpoint, EarlyStopping, LearningRateScheduler, TensorBoard
 from keras.layers import Dense, Dropout, Flatten, AveragePooling2D
 from keras.models import Model
 from keras.preprocessing.image import ImageDataGenerator
@@ -14,6 +14,15 @@ from keras_sequential_ascii import keras2ascii
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
+
+'''
+  set global configuratin
+  num_classes - total number of classes in dataset. In our case we have 10
+  batch_size  - how many images we process in single batch
+  epochs - number of iterations over whole dataset
+  
+  
+'''
 num_classes = 10
 batch_size = 16
 epochs = 500
@@ -27,15 +36,20 @@ def get_num_of_files(root_dir):
 
 
 def retrain(train_data_dir, validation_data_dir):
+    # image size for input layer
     img_width, img_height = 299, 299
 
+    #get number of samples in train dir
     nb_train_samples = get_num_of_files(train_data_dir)
+    #get number of samples in validation dir
     nb_validation_samples = get_num_of_files(validation_data_dir)
 
+    # we are going to use InceptionV3 pretrained model and apply transfer learning approach
     base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(img_width, img_height, 3))
 
     x = base_model.output
 
+    #create and initialize output layer
     x = Dense(128, activation='relu', init='glorot_uniform', W_regularizer=l2(.0005))(x)
     x = Dropout(0.7)(x)
 
@@ -46,10 +60,13 @@ def retrain(train_data_dir, validation_data_dir):
 
     model = Model(inputs=base_model.input, outputs=predictions)
 
+    #print model summary
     print(keras2ascii(model))
+
 
     opt = optimizers.SGD(lr=.01, momentum=.9)
 
+    # define learning rate decay strategy
     def schedule(epoch):
         if epoch < 20:
             return 0.01
@@ -62,6 +79,7 @@ def retrain(train_data_dir, validation_data_dir):
 
     model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
+    #create train image generator with augmentation
     train_datagen = ImageDataGenerator(
         rescale=1. / 255,
         shear_range=0.3,
@@ -79,6 +97,7 @@ def retrain(train_data_dir, validation_data_dir):
         batch_size=batch_size,
         class_mode="categorical")
 
+    #create train image generator
     test_datagen = ImageDataGenerator(
         rescale=1. / 255,
     )
@@ -89,13 +108,20 @@ def retrain(train_data_dir, validation_data_dir):
         batch_size=256,
         class_mode="categorical")
 
+    #define checkpoint, so our model would be saved if conditions are met (best models only)
     checkpoint = ModelCheckpoint(filepath='model.{epoch:02d}-{val_loss:.2f}-{val_acc:.2f}.hdf5', monitor='val_acc',
                                  verbose=2, save_best_only=True,
                                  save_weights_only=False,
                                  mode='auto', period=1)
 
+    #stop training if there are no improvements after 50 epochs
     early = EarlyStopping(monitor='val_acc', min_delta=0, patience=50, verbose=1, mode='auto')
 
+    #write logs for tensorboard
+    tensorboard = TensorBoard(log_dir='/tmp/tb_logs/metrics', histogram_freq=2, write_graph=True, write_images=False)
+    tensorboard.set_model(model)
+
+    #start training
     model.fit_generator(
         train_generator,
         steps_per_epoch=nb_train_samples / batch_size,
@@ -103,7 +129,7 @@ def retrain(train_data_dir, validation_data_dir):
         epochs=epochs,
         use_multiprocessing=True,
         validation_data=validation_generator,
-        callbacks=[lr_scheduler, early, checkpoint])
+        callbacks=[lr_scheduler, early, checkpoint, tensorboard])
 
 
 if __name__ == '__main__':
