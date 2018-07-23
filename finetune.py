225a8e67775d99a9211e2937f8c976c1d7a8aa68
David Mayboroda <david.ftzi@gmail.com>
2018-07-23 19:35:16 +0300

    commented finetune

--- finetune.py
+++ finetune.py
@@ -9,7 +9,16 @@ batch_per_gpu = 16
 num_gpus = 2
 batch_size = batch_per_gpu * num_gpus
 
+'''
+Creating two iterators (for training and validation data set parts)
+Data shape that used for iteration = 3 * 299 * 299 where:
+3 - dimension (RGB)
+299 * 299 - width and height of images.
 
+Params:
+batch size - batch per gpu (16) * number of gpu (2)
+Data shape = 3 * 299 * 299
+'''
 def get_iterators(batch_size, data_shape=(3, 299, 299)):
     train = mx.io.ImageRecordIter(
         path_imgrec='./data-train.rec',
@@ -31,6 +40,20 @@ def get_iterators(batch_size, data_shape=(3, 299, 299)):
     return train, val
 
 
+'''
+Main transfer learning method.
+Fetching all layers of current model (Inception-BN)
+Finding layer called "flatten_output"
+Then adding activation + dropout + fully connected layers
+At the end added softmax output
+Relu as activation function
+Dropout with always mode (not only while training), 0.7 - dropout fraction
+return all arguments of pre-trained model and architecture as 'net'.
+
+Params:
+symbol - configuration of network
+arg_params - network weights
+'''
 def do_finetune(symbol, arg_params):
     all_layers = symbol.get_internals()
     net = all_layers["flatten_output"]
@@ -42,6 +65,20 @@ def do_finetune(symbol, arg_params):
     return net, new_args
 
 
+'''
+Training method
+Initialize number of contexts to train on (2 gpus)
+Creating a metrics (accuracy and cross-entropy)
+Training new weights with 100 epoch
+Optimizer - SGD (with learning rate 0.01, momentum - 0.9, weight decay - 0.0005)
+
+Params:
+symbol - configuration of network
+arg_params - network weights
+aux_params - network auxiliary states
+train - training iterator
+val - validation iterator
+'''
 def fit(symbol, arg_params, aux_params, train, val):
     devs = [mx.gpu(i) for i in range(num_gpus)]
     mod = mx.mod.Module(symbol=symbol, context=devs)
