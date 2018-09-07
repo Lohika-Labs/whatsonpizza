88938b052f799cf6b9855ea6f88ffc04c802685b
David Mayboroda <david.ftzi@gmail.com>
2018-09-07 15:42:07 +0300

    Update imgtool.sh

--- imgtool.sh
+++ imgtool.sh
@@ -2,9 +2,9 @@
 
 CMD="python3 -m mxnet.tools.im2rec"
 
-${CMD} --list --recursive --num-thread 16 data-train /home/vbartko/projects/wop_data/train/
-${CMD} --list --recursive --num-thread 16 data-val /home/vbartko/projects/wop_data/test/
-${CMD} --list --recursive --num-thread 16 data-test /home/vbartko/projects/wop_data/orig/
-${CMD} --resize 299 --quality 90 --num-thread 16 data-val /home/vbartko/projects/wop_data/test/
-${CMD} --resize 299 --quality 90 --num-thread 16 data-train /home/vbartko/projects/wop_data/train/
-${CMD} --resize 299 --quality 90 --num-thread 16 data-test /home/vbartko/projects/wop_data/orig/
+${CMD} --list --recursive --num-thread 16 data-train ../wop_data/train/
+${CMD} --list --recursive --num-thread 16 data-val ../wop_data/test/
+${CMD} --list --recursive --num-thread 16 data-test ../wop_data/orig/
+${CMD} --resize 299 --quality 90 --num-thread 16 data-val ../wop_data/test/
+${CMD} --resize 299 --quality 90 --num-thread 16 data-train ../wop_data/train/
+${CMD} --resize 299 --quality 90 --num-thread 16 data-test ../wop_data/orig/
